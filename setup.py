c688eeb012316c6f72695c3b96eb2ea730d307eb
Jiajun <jiajunb@umich.edu>
2019-11-05 21:07:45 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Fine-tune BERT
+"""
+
+from setuptools import setup
+
+setup(
+    name='MAC',
+    version='0.1.0',
+    description='Test code from the author',
+    packages=[],
+    include_package_data=True,
+    install_requires=[
+        'tensorflow==1.14.0',
+        'sklearn',
+        'scipy'
+    ],
+)
